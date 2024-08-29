c21456f174948fddc2e33d829af00329f9cb47ae
binwoonki <vinwoonki98@gmail.com>
2024-08-29 18:22:58 +0900

    input 검색어 입력시 이미지 받아오기

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 import products from "./product.js";
 
-const filteredProducts = [...products];
+let filteredProducts = [...products];
 
 const productsContainer = document.querySelector(".products-container");
+const formEl = document.querySelector(".input-form");
+const searchInput = document.querySelector(".search-input");
+
 window.addEventListener("DOMContentLoaded", () => {
   displayMenu(filteredProducts);
 });
 
-function displayMenu(menuItems) {
-  let displayMenu = menuItems.map((product) => {
-    return (productsContainer.innerHTML = `
+formEl.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  const inputValue = searchInput.value;
+
+  const result = filteredProducts.filter((item) => {
+    return item.title.toLocaleLowerCase().includes(inputValue);
+  });
+
+  displayMenu(result);
+});
+
+function displayMenu(menuItem) {
+  productsContainer.innerHTML = menuItem
+    .map((product) => {
+      return (productsContainer.innerHTML = `
   <article>
     <img
       src=${product.image}
@@ -21,7 +36,6 @@ function displayMenu(menuItems) {
       <span class="product-price">${product.price}</span>
     </div>
   </article>`);
-  });
-
-  productsContainer.innerHTML = displayMenu.join("");
+    })
+    .join("");
 }
