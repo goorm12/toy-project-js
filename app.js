e5328b6e49f588e59c0dc3a3d2e562e166ca69ee
binwoonki <vinwoonki98@gmail.com>
2024-08-29 11:49:52 +0900

    js 작업 완료

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 import getUser from "./utils/fetchUser.js";
+import displayUser from "./utils/displayUser.js";
+import getElement from "./utils/getElement.js";
 
-window.document.addEventListener("DOMContentLoaded", getUser);
+const btn = getElement(".btn");
+
+const showUser = async () => {
+  const person = await getUser();
+
+  displayUser(person);
+};
+
+window.document.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);
