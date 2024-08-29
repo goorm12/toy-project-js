e2f3e2c08ef2e0897eced16ec01a467cd58a17fd
binwoonki <vinwoonki98@gmail.com>
2024-08-29 17:01:57 +0900

    스타일 적용 및 화면에 이미지 뿌리기

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 import products from "./product.js";
+
+const filteredProducts = [...products];
+
+const productsContainer = document.querySelector(".products-container");
+window.addEventListener("DOMContentLoaded", () => {
+  displayMenu(filteredProducts);
+});
+
+function displayMenu(menuItems) {
+  let displayMenu = menuItems.map((product) => {
+    return (productsContainer.innerHTML = `
+  <article>
+    <img
+      src=${product.image}
+      alt="product-img"
+      class="product-img img"
+    />
+    <div>
+      <h5 class="product-name">${product.title}</h5>
+      <span class="product-price">${product.price}</span>
+    </div>
+  </article>`);
+  });
+
+  productsContainer.innerHTML = displayMenu.join("");
+}
