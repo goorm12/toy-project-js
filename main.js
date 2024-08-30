e90387730d8f14cda31850618fd7bb6f1b6ba5f4
binwoonki <vinwoonki98@gmail.com>
2024-08-30 14:27:24 +0900

    slide 버튼 이벤트 작업 완료 클릭시 pause 및 play

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const slideBtnEl = document.querySelector(".switch-btn");
+const videoEl = document.querySelector(".video-container");
+
 window.addEventListener("load", () => {
   document.querySelector(".preloader").classList.add("hide-preloader");
+});
+
+slideBtnEl.addEventListener("click", () => {
+  if (!slideBtnEl.classList.contains("slide")) {
+    slideBtnEl.classList.add("slide");
+    videoEl.pause();
+  } else {
+    slideBtnEl.classList.remove("slide");
+    videoEl.play();
+  }
 });
